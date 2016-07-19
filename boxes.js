ff4b856ecd6dc106b622f8b50c46cdb8d33c4885
Alex Nye <alnye655321@gmail.com>
2016-07-18 21:26:48 -0600

    init commit

--- boxes.js
+++ boxes.js
@@ -1,1 +1,24 @@
-console.log("hello world");
+$( document ).ready(function() {
+    alert( "DOM Ready!" );
+
+// var $secret = $('#secretBox');
+
+//Exercise 2
+
+//1)
+$('#secretBox').css("background-color", "white");
+
+//2)
+$('#row1 div').addClass("boxType3");
+
+//3)
+$('#row4 div:nth-child(4)').css("display" , "none");
+
+//4)
+$('.boxType1').css('background-color','white');
+$('#container div div').removeClass('boxType1');
+
+//p:nth-child(4)
+
+
+});
